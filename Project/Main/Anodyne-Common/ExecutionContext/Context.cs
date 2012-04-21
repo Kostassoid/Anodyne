e46073d1122ee7ba23771644179f714eb6c22e25
Kostasoid <k.alexandroff@gmail.com>
2012-04-21 04:46:28 +0400

    serious fixes in Context. added some tests for Context.

--- Project/Main/Anodyne-Common/ExecutionContext/Context.cs
+++ Project/Main/Anodyne-Common/ExecutionContext/Context.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Kostassoid.Anodyne.Common.ExecutionContext
 {
@@ -6,17 +7,18 @@ namespace Kostassoid.Anodyne.Common.ExecutionContext
     {
         private static IContextProvider _provider = new NormalContextProvider();
 
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public static void SetProvider(IContextProvider provider)
         {
             _provider = provider;
         }
 
-        public void Set(string name, object value)
+        public static void Set(string name, object value)
         {
             _provider.Set(name, value);
         }
 
-        public object Get(string name)
+        public static object Get(string name)
         {
             var found = _provider.Find(name);
             if (found == null)
@@ -25,17 +27,21 @@ namespace Kostassoid.Anodyne.Common.ExecutionContext
             return found;
         }
 
-        public T GetAs<T>(string name) where T : class
+        public static T GetAs<T>(string name) where T : class
         {
-            return Get(name) as T;
+            var found = Get(name);
+            if (found as T == null)
+                throw new InvalidOperationException(string.Format("Value of '{0}' is of type {1}, but {2} was expected", name, found.GetType().Name, typeof(T).Name));
+
+            return found as T;
         }
 
-        public Option<object> Find(string name)
+        public static Option<object> Find(string name)
         {
             return _provider.Find(name).AsOption();
         }
 
-        public Option<T> FindAs<T>(string name) where T : class
+        public static Option<T> FindAs<T>(string name) where T : class
         {
             var found = Find(name);
             if (found.IsNone) return new None<T>();
@@ -43,7 +49,7 @@ namespace Kostassoid.Anodyne.Common.ExecutionContext
             return (found.Value as T).AsOption();
         }
 
-        public void Release(string name)
+        public static void Release(string name)
         {
             _provider.Release(name);
         }
---
+++ Project/Specs/Anodyne-Common-Specs/ExecutionContextSpecs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Kostassoid.Anodyne.Common.ExecutionContext;
+using NUnit.Framework;
+
+namespace Kostassoid.Anodyne.Common.Specs
+{
+    // ReSharper disable InconsistentNaming
+    public class ExecutionContextSpecs
+    {
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_setting_new_value
+        {
+            [Test]
+            public void it_should_be_stored_and_available()
+            {
+                Context.Set("test", "zzz");
+
+                Assert.That(Context.Get("test"), Is.EqualTo("zzz"));
+                Assert.That(Context.GetAs<string>("test"), Is.EqualTo("zzz"));
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_resetting_value
+        {
+            [Test]
+            public void old_value_should_be_replaced()
+            {
+                Context.Set("test", "zzz");
+                Context.Set("test", 123);
+
+                Assert.That((int)Context.Get("test"), Is.EqualTo(123));
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_getting_non_existing_value
+        {
+            [Test]
+            public void should_throw()
+            {
+                Context.Set("test", "zzz");
+
+                Assert.Throws<InvalidOperationException>(() => Context.Get("testzzz"));
+                Assert.Throws<InvalidOperationException>(() => Context.GetAs<string>("testzzz"));
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_finding_non_existing_value
+        {
+            [Test]
+            public void should_return_none()
+            {
+                Context.Set("test", "zzz");
+
+                Assert.That(Context.Find("testzzz").IsNone, Is.True);
+                Assert.That(Context.FindAs<string>("testzzz").IsNone, Is.True);
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_finding_existing_value
+        {
+            [Test]
+            public void should_return_some()
+            {
+                Context.Set("test", "zzz");
+
+                Assert.That(Context.Find("test").Value, Is.EqualTo("zzz"));
+                Assert.That(Context.FindAs<string>("test").Value, Is.EqualTo("zzz"));
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_getting_released_value
+        {
+            [Test]
+            public void should_return_some()
+            {
+                Context.Set("test", "zzz");
+                Context.Release("test");
+
+                Assert.Throws<InvalidOperationException>(() => Context.Get("test"));
+            }
+        }
+
+
+
+
+    }
+    // ReSharper restore InconsistentNaming
+
+}
