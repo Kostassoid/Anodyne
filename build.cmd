bd70ffbaae68219c4a30e3a8a3cb1803f6bc6d53
Kostassoid <k.alexandroff@gmail.com>
2012-05-03 15:57:30 +0400

    Added build script + snk. Fixed Anodyne-Domain assembly name.

---
+++ build.cmd
@@ -0,0 +1,27 @@
+@echo off
+set path=%path%;C:/Windows/Microsoft.NET/Framework/v4.0.30319;
+
+echo Building project...
+msbuild src/Anodyne.sln /nologo /v:q /p:Configuration=Release /t:Clean
+msbuild src/Anodyne.sln /nologo /v:q /p:Configuration=Release /clp:ErrorsOnly
+
+echo Merging assemblies...
+if exist output rmdir /s /q output
+mkdir output
+mkdir output\bin
+
+tools\ilmerge\ILMerge.exe /keyfile:src\Anodyne.snk /internalize /wildcards /target:library ^
+ /targetplatform:"v4,C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319" ^
+ /out:"output\bin\Anodyne-Core.dll" ^
+ "src\main\Anodyne-Common\bin\Release\Anodyne-Common.dll" ^
+ "src\main\Anodyne-System\bin\Release\Anodyne-System.dll" ^
+ "src\main\Anodyne-Domain\bin\Release\Anodyne-Domain.dll" ^
+ "src\main\Anodyne-Wiring\bin\Release\Anodyne-Wiring.dll" ^
+ "src\main\Anodyne-DataAccess\bin\Release\Anodyne-DataAccess.dll" ^
+ "src\main\Anodyne-CommandBus\bin\Release\Anodyne-CommandBus.dll"
+
+copy src\main\Anodyne-Windsor\bin\Release\Anodyne-Windsor.* output\bin
+copy src\main\Anodyne-MongoDb\bin\Release\Anodyne-MongoDb.* output\bin
+copy src\main\Anodyne-Log4Net\bin\Release\Anodyne-Log4Net.* output\bin
+ 
+echo Done.
--- src/specs/Anodyne-Domain-Specs/Properties/AssemblyInfo.cs
+++ src/specs/Anodyne-Domain-Specs/Properties/AssemblyInfo.cs
@@ -1,36 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Anodyne-Domain-Specs")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Anodyne-Domain-Specs")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+﻿using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5212c221-b6e2-4cc6-b087-cfcf25bad6c4")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
