2a24fad250e3c5aa90920d8f4e54f60602215e39
ialekseev <alekseevi15@gmail.com>
2013-02-10 15:44:43 +0300

    New Anodyne-Web-Mvc4 project to deal with Mvc4 WebApi

---
+++ src/main/Anodyne-Web-Mvc4/ContainerWebApiDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kostassoid.Anodyne.Abstractions.Dependency;
+
+namespace Kostassoid.Anodyne.Web.Mvc4
+{
+    public class ContainerWebApiDependencyResolver : System.Web.Http.Dependencies.IDependencyResolver 
+    {
+        private readonly IContainer _container;
+
+        public ContainerWebApiDependencyResolver(IContainer container)
+        {
+            _container = container;
+        }
+        
+        public System.Web.Http.Dependencies.IDependencyScope BeginScope()
+        {
+            return this;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _container.Has(serviceType) ? _container.Get(serviceType) : null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.Has(serviceType) ? _container.GetAll(serviceType).Cast<object>() : new object[] { };
+        }
+
+        public void Dispose(){}
+    }
+}
