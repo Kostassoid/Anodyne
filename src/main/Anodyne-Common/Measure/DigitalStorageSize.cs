bb7a15edbd04abf2f939d95fd94bb3c70ee2531d
Kostassoid <k.alexandroff@gmail.com>
2013-04-09 23:42:50 +0400

    Metrics (digital size) specs.

--- src/main/Anodyne-Common/Measure/DigitalStorageSize.cs
+++ src/main/Anodyne-Common/Measure/DigitalStorageSize.cs
@@ -1,12 +1,60 @@
-﻿namespace Kostassoid.Anodyne.Common.Measure
+﻿// Copyright 2011-2013 Anodyne.
+//   
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0 
+//  
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+
+namespace Kostassoid.Anodyne.Common.Measure
 {
     public class DigitalStorageSize
     {
         public long Bytes { get; private set; }
+        public long Kilobytes { get { return Bytes / 1024; } }
+        public long Megabytes { get { return Bytes / (1024 * 1024); } }
 
-        public DigitalStorageSize(long bytes)
+        private DigitalStorageSize(long bytes)
         {
             Bytes = bytes;
+        }
+
+        public static DigitalStorageSize FromBytes(long bytes)
+        {
+            return new DigitalStorageSize(bytes);
+        }
+
+        public static DigitalStorageSize FromKilobytes(long kilobytes)
+        {
+            return new DigitalStorageSize(kilobytes * 1024);
+        }
+
+        public static DigitalStorageSize FromMegabytes(long megabytes)
+        {
+            return new DigitalStorageSize(megabytes * 1024 * 1024);
+        }
+
+        protected bool Equals(DigitalStorageSize other)
+        {
+            return Bytes == other.Bytes;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DigitalStorageSize) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Bytes.GetHashCode();
         }
     }
 }
---
+++ src/specs/Anodyne-Common-Specs/MetricsSpecs.cs
@@ -0,0 +1,145 @@
+﻿// Copyright 2011-2013 Anodyne.
+//   
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0 
+//  
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+
+namespace Kostassoid.Anodyne.Common.Specs
+{
+    using FluentAssertions;
+    using Measure;
+    using NUnit.Framework;
+
+    // ReSharper disable InconsistentNaming
+    public class MetricsSpecs
+    {
+        [TestFixture]
+        [Category("Unit")]
+        public class when_creating_digital_size_from_bytes
+        {
+            [Test]
+            public void should_store_as_is()
+            {
+                var size = DigitalStorageSize.FromBytes(123456789L);
+
+                size.Bytes.Should().Be(123456789L);
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_creating_digital_size_from_kilobytes
+        {
+            [Test]
+            public void should_store_as_bytes()
+            {
+                var size = DigitalStorageSize.FromKilobytes(123L);
+
+                size.Bytes.Should().Be(123L * 1024);
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_creating_digital_size_from_megabytes
+        {
+            [Test]
+            public void should_store_as_bytes()
+            {
+                var size = DigitalStorageSize.FromKilobytes(123L);
+
+                size.Bytes.Should().Be(123L * 1024);
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_comparing_same_sized_objects
+        {
+            [Test]
+            public void should_be_equal()
+            {
+                var size1 = DigitalStorageSize.FromBytes(123L * 1024L);
+                var size2 = DigitalStorageSize.FromKilobytes(123L);
+
+                size1.Bytes.Should().Be(size2.Bytes);
+                size1.Should().Be(size2);
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_creating_using_int32_extension_methods
+        {
+            [Test]
+            public void should_be_equivalent_to_direct_methods()
+            {
+                var size1 = DigitalStorageSize.FromBytes(123456789L);
+                var size2 = 123456789.Bytes();
+                size1.Bytes.Should().Be(size2.Bytes);
+
+                size1 = DigitalStorageSize.FromKilobytes(123L);
+                size2 = 123.Kilobytes();
+                size1.Bytes.Should().Be(size2.Bytes);
+
+                size1 = DigitalStorageSize.FromMegabytes(123L);
+                size2 = 123.Megabytes();
+                size1.Bytes.Should().Be(size2.Bytes);
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_creating_using_int64_extension_methods
+        {
+            [Test]
+            public void should_be_equivalent_to_direct_methods()
+            {
+                var size1 = DigitalStorageSize.FromBytes(123456789L);
+                var size2 = 123456789L.Bytes();
+                size1.Bytes.Should().Be(size2.Bytes);
+
+                size1 = DigitalStorageSize.FromKilobytes(123L);
+                size2 = 123L.Kilobytes();
+                size1.Bytes.Should().Be(size2.Bytes);
+
+                size1 = DigitalStorageSize.FromMegabytes(123L);
+                size2 = 123L.Megabytes();
+                size1.Bytes.Should().Be(size2.Bytes);
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class when_getting_size_in_different_metrics
+        {
+            [Test]
+            [TestCase(123456789L)]
+            [TestCase(1024L)]
+            [TestCase(1023L)]
+            [TestCase(0L)]
+            [TestCase(1L)]
+            [TestCase(333L)]
+            public void should_return_closest_values_according_to_metric_scale(long bytes)
+            {
+                var size = DigitalStorageSize.FromBytes(bytes);
+
+                var kb = size.Kilobytes;
+                (kb * 1024).Should().BeInRange(size.Bytes - 1024, size.Bytes);
+
+                var mb = size.Megabytes;
+                (mb * 1024 * 1024).Should().BeInRange(size.Bytes - (1024 * 1024), size.Bytes);
+            }
+        }
+
+    }
+    // ReSharper restore InconsistentNaming
+
+}
